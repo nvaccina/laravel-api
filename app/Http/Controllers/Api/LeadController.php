a9a7e6bfe5b3681f543fd4967d0d37bbf0c8a2e0
nvaccina <niccolovac@gmail.com>
2023-06-29 14:55:43 +0200

    Add Validator

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -4,10 +4,38 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class LeadController extends Controller
 {
-    //$data = $request->all();
+    public function store(Request $request){
 
-    //return response()->json($data);
+        $data = $request->all();
+
+        $validator = Validator::make( $data,
+            [
+                'name' => 'required|min:2|max:255',
+                'email' => 'required|email|max:255',
+                'message' => 'required|min:10',
+            ],
+            [
+                'name.required' => 'Il nome è un campo obbligatorio',
+                'name.min' => 'Il nome deve contenere almeno :min caratteri',
+                'name.max' => 'Il nome non può avere più di :max caratteri',
+                'email.required' => 'La mail è un campo obbligatorio',
+                'email.mail' => 'L\'indirizzo email non corretto',
+                'email.max' => 'La mail non può avere più di :max caratteri',
+                'message.required' => 'Il messaggio è un campo obbligatorio',
+                'message.min' => 'Il messaggio deve contenere almeno :min caratteri',
+            ]
+        );
+
+        if($validator->fails()){
+            $success = false;
+            $errors =$validator->errors();
+        }
+
+        $success = true;
+        return response()->json(compact('success'));
+    }
 }
